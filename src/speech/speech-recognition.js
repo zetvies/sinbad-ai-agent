384be0ca403532f02dfe41a0b0b6d9942b9cbb52
Bimo Arsa <zetvies@gmail.com>
2024-07-04 18:22:03 +0700

    add data extraction demo

--- src/speech/speech-recognition.js
+++ src/speech/speech-recognition.js
@@ -1,18 +1,19 @@
-import { clearLog, logEvent } from './log.js';
+import { clearLog, logEvent } from "./log.js";
 
 export function disableSpeechRecognition() {
-  document.querySelector('.support').hidden = false;
-  document.querySelector('main').hidden = true;
+  document.querySelector(".support").hidden = false;
+  document.querySelector("main").hidden = true;
 }
 
-const confidence = document.querySelector('#confidence');
-const transcription = document.querySelector('#transcription');
+const confidence = document.querySelector("#confidence");
+const transcription = document.querySelector("#transcription");
 
 export function startSpeechRecognition(SpeechRecognition) {
   const recognizer = new SpeechRecognition();
+  recognizer.continuous = true;
 
-  recognizer.addEventListener('result', (event) => {
-    transcription.value = '';
+  recognizer.addEventListener("result", (event) => {
+    transcription.value = "";
 
     for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
       if (event.results[i].isFinal) {
@@ -24,35 +25,37 @@ export function startSpeechRecognition(SpeechRecognition) {
     }
   });
 
-  recognizer.addEventListener('error', (event) => {
+  recognizer.addEventListener("error", (event) => {
     logEvent(`Recognition error: ${event.message}`);
   });
 
-  recognizer.addEventListener('end', () => {
-    logEvent('Recognition ended');
+  recognizer.addEventListener("end", () => {
+    logEvent("Recognition ended");
   });
 
-  document.querySelector('#record').addEventListener('click', () => {
+  document.querySelector("#record").addEventListener("click", () => {
     startRecognition(recognizer);
   });
 
-  document.querySelector('#stop').addEventListener('click', () => {
+  document.querySelector("#stop").addEventListener("click", () => {
     stopRecognition(recognizer);
   });
 
-  document.querySelector('#clear').addEventListener('click', clearLog);
+  document.querySelector("#clear").addEventListener("click", clearLog);
 }
 
 function startRecognition(recognizer) {
-  const isInterim = document.querySelector('input[name="recognition-type"][value="interim"]').checked;
+  const isInterim = document.querySelector(
+    'input[name="recognition-type"][value="interim"]'
+  ).checked;
 
-  recognizer.lang = document.querySelector('#language').value;
+  recognizer.lang = document.querySelector("#language").value;
   recognizer.continuous = !isInterim;
   recognizer.interimResults = isInterim;
 
   try {
     recognizer.start();
-    logEvent('Recognition started');
+    logEvent("Recognition started");
   } catch (error) {
     logEvent(`Recognition error: ${error.message}`);
   }
@@ -60,5 +63,5 @@ function startRecognition(recognizer) {
 
 function stopRecognition(recognizer) {
   recognizer.stop();
-  logEvent('Recognition stopped');
+  logEvent("Recognition stopped");
 }
